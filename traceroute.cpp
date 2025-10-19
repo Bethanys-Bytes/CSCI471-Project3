d59c655db4f445e6936aba69ed0268cbbd60655e
Phil Romig <promig3@cse.mines.edu>
2025-10-19 13:22:11 -0600

    Initial commit

---
+++ traceroute.cpp
@@ -0,0 +1,46 @@
+#include "traceroute.h"
+
+// ****************************************************************************
+// * Compute the Internet Checksum over an arbitrary buffer.
+// * (written with the help of ChatGPT 3.5)
+// ****************************************************************************
+uint16_t checksum(unsigned short *buffer, int size) {
+    unsigned long sum = 0;
+    while (size > 1) {
+        sum += *buffer++;
+        size -= 2;
+    }
+    if (size == 1) {
+        sum += *(unsigned char *) buffer;
+    }
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    return (unsigned short) (~sum);
+}
+
+
+int main (int argc, char *argv[]) {
+  std::string destIP;
+
+  // ********************************************************************
+  // * Process the command line arguments
+  // ********************************************************************
+  int opt = 0;
+  while ((opt = getopt(argc,argv,"d:v:")) != -1) {
+
+    switch (opt) {
+    case 'd':
+      destIP = optarg;
+      break;
+    case 'v':
+      LOG_LEVEL = atoi(optarg);;
+      break;
+    case ':':
+    case '?':
+    default:
+      std::cout << "useage: " << argv[0] << " -d [destination ip] -v [Log Level]" << std::endl;
+      exit(-1);
+    }
+  }
+
+}
