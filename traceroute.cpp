f5499f5793bbd9a657741dd95076cc7e003b703c
Bethanys-Bytes <boehmerbethany@gmail.com>
2025-10-28 17:30:24 -0600

    permission denied solved, now trouble w/ empty reply

--- traceroute.cpp
+++ traceroute.cpp
@@ -19,6 +19,39 @@ uint16_t checksum(unsigned short *buffer, int size) {
     return (unsigned short) (~sum);
 }
 
+bool isValidIpAddress(const char *ipAddress) {
+	struct sockaddr_in sa;
+	int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+	return result != 0;
+}
+
+void fill_in_IP_header(char *sendBuffer, int current_ttl, std::string destIP) {
+// 3. Fill in all the fields of the IP header at the front of the buffer.
+	// a. You don’t need to fill in source IP or checksum
+	struct iphdr *ip = (struct iphdr *)sendBuffer;
+	ip->version = 4;
+	ip->ihl = 5;
+	ip->tos = 0;
+	ip->tot_len = htons(sizeof(sendBuffer));
+	ip->id = htons(0);
+	ip->frag_off = 0;
+	ip->protocol = IPPROTO_ICMP;
+	ip->daddr = inet_addr(destIP.c_str());
+	ip->ttl = current_ttl;
+	ip->check = 0;
+	ip->saddr = 0;
+}
+
+void fill_in_ICMP_header(char *sendBuffer) {
+// 4. Fill in all the fields of the ICMP header right behind the IP header.
+	struct icmphdr *icmp = (struct icmphdr *)(sendBuffer + sizeof(struct iphdr));
+	icmp->type = ICMP_ECHO;
+	icmp->code = 0;
+	icmp->un.echo.id = htons(getpid() & 0xFFFF);
+	icmp->un.echo.sequence = htons(1);
+	icmp->checksum = checksum((unsigned short *)icmp, sizeof(struct icmphdr));
+}
+
 
 int main (int argc, char *argv[]) {
   std::string destIP;
@@ -44,55 +77,19 @@ int main (int argc, char *argv[]) {
     }
   }
 
-	if (destIP.compare("")) {
-		DEBUG << "no destination specified" << ENDL;
-		return -1;
+	if (isValidIpAddress(destIP.c_str()) < 0) {
+		std::cout << "Invalid" << std::endl;
 	}
-
-// 1. Allocate two 64 byte buffers. One for sending and one for receiving.
-	char sendBuffer[64];
-	char recvBuffer[64];
-
-// 2. Fill the whole buffer with a pattern of characters of your choice.
-	std::memset(sendBuffer, 'a', sizeof(sendBuffer));
-	std::memset(recvBuffer, 'a', sizeof(recvBuffer));
-
-// 3. Fill in all the fields of the IP header at the front of the buffer.
-	// a. You don’t need to fill in source IP or checksum
-	struct iphdr *ip = (struct iphdr *)sendBuffer;
-	ip->version = 4;
-	ip->ihl = 5;
-	ip->tos = 0;
-	ip->tot_len = htons(sizeof(sendBuffer));
-	ip->id = htons(0);
-	ip->frag_off = 0;
-	ip->protocol = IPPROTO_ICMP;
-	ip->daddr = inet_addr(destIP.c_str());
-	ip->check = 0;
-	ip->saddr = 0;
-
-// 4. Fill in all the fields of the ICMP header right behind the IP header.
-	struct icmphdr *icmp = (struct icmphdr *)(sendBuffer + sizeof(struct iphdr));
-	icmp->type = ICMP_ECHO;
-	icmp->code = 0;
-	icmp->un.echo.id = htons(getpid() & 0xFFFF);
-	icmp->un.echo.sequence = htons(1);
-	icmp->checksum = 0; // will adjust later in the actual loop
 
 // 5. Create the send and receive sockets.
 	int sendSock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (sendSock < 0) {
 		DEBUG << "problem creating send socket" << ENDL;
-		perror("sendSock");  // "Operation not permitted" error --> run with sudo
 		return -1;
 	}
 
-	// tell socket to use my IP header
-	int one = 1;
-	if (setsockopt(sendSock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
-		DEBUG << "couldn't override socket" << ENDL;
-		return -1;
-	}
+	// int one = 1;
+	// setsockopt(sendSock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one));
 
 	int recvSock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (recvSock < 0) {
@@ -104,71 +101,89 @@ int main (int argc, char *argv[]) {
 	bool no_reply = true;
 // 6. while (CURRENT_TTL <= 31) and (reply-not-received)
 	while (current_ttl <= 31 && no_reply) {
+	// 1. Allocate two 64 byte buffers. One for sending and one for receiving.
+		char sendBuffer[64];
+		char recvBuffer[64];
+
+	// 2. Fill the whole buffer with a pattern of characters of your choice.
+		std::memset(sendBuffer, 'a', sizeof(sendBuffer));
+		std::memset(recvBuffer, 'a', sizeof(recvBuffer));
 	// a. Set the TTL in the IP header in the buffer to CURRENT_TTL
-		ip->ttl = current_ttl;
+		fill_in_IP_header(sendBuffer, current_ttl, destIP);
 	// b. Set the checksum in the ICMP header
-		icmp->checksum = checksum((unsigned short *)icmp, sizeof(struct icmphdr));
+		fill_in_ICMP_header(sendBuffer);
 	// c. Send the buffer using sendfrom()
 		struct sockaddr_in destAddr{};
 		destAddr.sin_family = AF_INET;
-		destAddr.sin_addr.s_addr = ip->daddr;
+		destAddr.sin_addr.s_addr = inet_addr(destIP.c_str());
 
 		ssize_t sent_bytes = sendto(sendSock, sendBuffer, sizeof(sendBuffer), 0, (struct sockaddr *)&destAddr, sizeof(destAddr));
 		if (sent_bytes < 0) {
 			DEBUG << "send from socket failed" << ENDL;
-			perror("sendSock");
 			return -1;
 		}
 	// d. While (now < START_TIME + 15) and (not-done-reading)
-	struct timeval start, now;
-	gettimeofday(&start, NULL);
-	gettimeofday(&now, NULL);
-	bool not_done_reading = true;
-		while (now.tv_sec < start.tv_sec + 15 && not_done_reading) {
+		struct timeval start, now;
+		gettimeofday(&start, NULL);
+
+		DEBUG << "Trying packet with a TTL of: " << current_ttl << ENDL;
+
+		bool not_done_reading = true;
+		while (not_done_reading) {
+			// calculate elapsed time
+			gettimeofday(&now, NULL);
+			double elapsed = (now.tv_sec - start.tv_sec) + (now.tv_usec - start.tv_usec) / 1e6;
+
+			if (elapsed >= 15.0) {
+				DEBUG << "Time exceeded 15 seconds. Increasing TTL." << ENDL;
+				break;
+			}
+
 		// i. Use select() to sleep for up to 5 seconds, wake up if data arrives.
 			fd_set readfds;
 			FD_ZERO(&readfds);
 			FD_SET(recvSock, &readfds);
 
 			struct timeval tv;
-			tv.tv_sec = 5; 
+			tv.tv_sec = 5;
 			tv.tv_usec = 0;
+
 			int ready = select(recvSock + 1, &readfds, NULL, NULL, &tv);
 
-			if (ready < 0) {
-				DEBUG << "error with select()" << ENDL;
+			if (!(ready > 0)) {
+					continue;
 			}
 
-		// ii. If data has arrived, read it with recevfrom()
+		// ii. If data has arrived, read it with recvfrom()
+			DEBUG << "Data is arriving" << ENDL;
 			struct sockaddr_in recvAddr;
 			socklen_t addrlen = sizeof(recvAddr);
 			ssize_t recv_bytes = recvfrom(recvSock, recvBuffer, sizeof(recvBuffer), 0, (struct sockaddr *)&recvAddr, &addrlen);
+
+			DEBUG << "Data received " << recv_bytes << " bytes" << ENDL;
 			if (recv_bytes > 0) {
 				struct iphdr *recv_ip = (struct iphdr *)recvBuffer;
-				struct icmphdr *recv_icmp = (struct icmphdr *)(recvBuffer + recv_ip->ihl*4);
-				
-				// 1. If received data is Echo Reply from the destination
+				struct icmphdr *recv_icmp = (struct icmphdr *)(recvBuffer + recv_ip->ihl * 4);
+
+				DEBUG << "Buffer: " << recvBuffer << ENDL;
+				DEBUG << "ICMP code: " << recv_icmp->code << ENDL;
+				DEBUG << "ICMP check: " << recv_icmp->checksum << ENDL;
+				DEBUG << "ICMP buffer: " << recvBuffer[20] << ENDL;
+				DEBUG << "Reply type: " << recv_icmp->type << ENDL;
+				// problem with the reply type being empty 
+				char ip_str[INET_ADDRSTRLEN];
+				inet_ntop(AF_INET, &(recv_ip->saddr), ip_str, INET_ADDRSTRLEN);
+				DEBUG << "IP: " << ip_str << ENDL;
 				if (recv_icmp->type == ICMP_ECHOREPLY) {
-					// a. Print message
-						char ip_str[INET_ADDRSTRLEN];
-						inet_ntop(AF_INET, &(recv_ip->saddr), ip_str, INET_ADDRSTRLEN);
-						std::cout << "Reply received from " << ip_str << "\n";
-					// b. Set reply-not-received to false
-						no_reply = false;
-					// c. Set not-done-reading to false
-						not_done_reading = false;
-
-				// 2. If received data is TTL Time Exceeded; TTL
+					std::cout << "Reply received from " << ip_str << "\n";
+					not_done_reading = false;
 				} else if (recv_icmp->type == ICMP_TIME_EXCEEDED) {
-					//check if subtype is 0???
-					// a. print message
-					std::cout << "No response with a TTL of " << ip->ttl << "\n";
-					// b. Set not-done-reading to false
-						not_done_reading = false;
+					std::cout << "No response with a TTL of " << current_ttl << "\n";
+					not_done_reading = false;
 				}
+			} else {
+				DEBUG << "No data read." << ENDL;
 			}
-
-			gettimeofday(&now, NULL);
 		}
 
 		current_ttl++;
